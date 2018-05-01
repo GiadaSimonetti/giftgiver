bf8756cd208cdf3d6f2c9842447ed3962c7472da
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-05-01 20:53:12 +0100

    Add index.js page

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
-import setupTests from "./setupTests";
-import tempPolyfills from "./tempPolyfills";
+import setupTests from "./../setupTests";
+import tempPolyfills from "./../tempPolyfills";
 
 import App from "./App";
 
