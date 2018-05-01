73a5876edfaa1e25415993e284bde6ea26d9eae8
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-05-01 21:29:26 +0100

    Add BDD: description, beforeEach and AfterEach

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -5,22 +5,36 @@ import tempPolyfills from "./../tempPolyfills";
 
 import App from "./App";
 
-const app = shallow(<App />);
+describe("App", () => {
 
-it("renders correctly", () => {
-  expect(app).toMatchSnapshot();
-});
+  const app = shallow(<App />);
 
-it("initilizes the 'state' as a empty list of gifts", () => {
-  expect(app.state().gifts).toEqual([]);
-});
+  it("renders correctly", () => {
+    expect(app).toMatchSnapshot();
+  });
 
-it("adds a new gift to 'state' when click 'Add Gift' button", () => {
-  app.find(".btn-add").simulate("click");
-  expect(app.state().gifts).toEqual([{ id: 1 }]);
-});
+  it("initilizes the 'state' as a empty list of gifts", () => {
+    expect(app.state().gifts).toEqual([]);
+  });
 
-it("adds a new gift to the rendered list when clicking the 'Add Gift' button", () => {
-  app.find(".btn-add").simulate("click");
-  expect(app.find(".gift-list").children().length).toEqual(2);
+  describe("when clicking the 'Add Gift' button", () => {
+
+    beforeEach(() => {
+      app.find(".btn-add").simulate("click");
+    });
+
+    afterEach(() => {
+      app.setState({ gifts : [] });
+    });
+
+    it("adds a new gift to 'state'", () => {
+      expect(app.state().gifts).toEqual([{ id: 1 }]);
+    });
+
+    it("adds a new gift to the rendered list", () => {
+      expect(app.find(".gift-list").children().length).toEqual(1);
+    });
+
+  });
+
 });
